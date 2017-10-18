24c740d95eab2d753491558c9f6aac391ef12e1e
Kinesum <mike.wilson983@gmail.com>
2017-10-18 13:44:15 -0600

    Responsive Calendar

--- js/MouseEvent.js
+++ js/MouseEvent.js
@@ -1,4 +1,51 @@
-function mouse_position(e)
+window.onresize = resize;
+
+function resize()
 {
-console.log("mouse location:", e.clientX, e.clientY)
+adapt();
+}
+
+function adapt(){
+if ($(window).height() < 368) {
+    // do something for small screens
+    console.log("reloading iframe to 300")
+    document.getElementById("calendarFrame").style.height = "300px";
+
+}
+else if ($(window).height() >= 368 &&  $(window).height() <= 468) {
+    // do something for medium screens
+    console.log("reloading iframe to 400")
+    document.getElementById("calendarFrame").style.height = "400px";
+
+}
+else if ($(window).height() >= 468 &&  $(window).height() <= 568) {
+    // do something for medium screens
+    console.log("reloading iframe to 500")
+    document.getElementById("calendarFrame").style.height = "500px";
+
+}
+else if ($(window).height() >= 568 &&  $(window).height() <= 668) {
+    // do something for medium screens
+    console.log("reloading iframe to 600")
+    document.getElementById("calendarFrame").style.height = "600px";
+
+}
+else if ($(window).height() >= 668 &&  $(window).height() <= 768) {
+    // do something for medium screens
+    console.log("reloading iframe to 600")
+    document.getElementById("calendarFrame").style.height = "700px";
+
+}
+else if ($(window).height() >= 768 &&  $(window).height() <= 868) {
+    // do something for medium screens
+    console.log("reloading iframe to 700")
+    document.getElementById("calendarFrame").style.height = "800px";
+
+}
+else {
+    // do something for medium screens
+    console.log("reloading iframe to 900")
+    document.getElementById("calendarFrame").style.height = "900px";
+
+}
 }
